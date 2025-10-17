ed23eb9780ea2c48c06d1de81291d143d1830497
TamseSaso <saso.tamse@scv.si>
2025-10-17 14:58:04 +0200

    non-blocking queues

--- utils/led_grid_visualizer.py
+++ utils/led_grid_visualizer.py
@@ -16,13 +16,19 @@ class LEDGridVisualizer(dai.node.ThreadedHostNode):
         
         self.input = self.createInput()
         self.input.setPossibleDatatypes([(dai.DatatypeEnum.Buffer, True)])
-        self.input.setQueueSize(1)
-        self.input.setBlocking(False)
+        try:
+            self.input.setQueueSize(1)
+            self.input.setBlocking(False)
+        except AttributeError:
+            pass
         
         self.out = self.createOutput()
         self.out.setPossibleDatatypes([(dai.DatatypeEnum.ImgFrame, True)])
-        self.out.setQueueSize(1)
-        self.out.setBlocking(False)
+        try:
+            self.out.setQueueSize(1)
+            self.out.setBlocking(False)
+        except AttributeError:
+            pass
         
         self.output_w, self.output_h = output_size
         self.grid_size = grid_size
