796953356f3d992657e61e1debedabb853c4660c
TamseSaso <saso.tamse@scv.si>
2025-11-04 12:21:15 +0100

    adjusted main_short

--- main_short.py
+++ main_short.py
@@ -12,8 +12,8 @@ SET_MANUAL_EXPOSURE = True  # Set to True to use manual exposure settings
 DEVICE_INFOS = [dai.DeviceInfo(ip) for ip in ["10.12.211.82", "10.12.211.84"]] # The master camera needs to be first here
 assert len(DEVICE_INFOS) > 1, "At least two devices are required for this example."
 # If SAMPLE is None then it checks every frame for synchronization
-SAMPLE = 0.1
-DEBUG = False
+SAMPLE = 1.0
+DEBUG = True
 
 # ---------------------------------------------------------------------------
 # Pipeline creation (unchanged API – only uses TARGET_FPS constant)
