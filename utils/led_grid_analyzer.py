6efee025072a3201a342aef21a37e0671b31a002
TamseSaso <saso.tamse@scv.si>
2025-11-07 12:01:59 +0100

    adjusted thresholds

--- utils/led_grid_analyzer.py
+++ utils/led_grid_analyzer.py
@@ -11,7 +11,7 @@ class LEDGridAnalyzer(dai.node.ThreadedHostNode):
     Output: dai.Buffer containing grid state data
     """
 
-    def __init__(self, grid_size: int = 32, threshold_multiplier: float = 1.6, bottom_row_threshold_scale: float = 0.7, debug: bool = False) -> None:
+    def __init__(self, grid_size: int = 32, threshold_multiplier: float = 1.5, bottom_row_threshold_scale: float = 0.68, debug: bool = False) -> None:
         super().__init__()
         
         self.input = self.createInput()
